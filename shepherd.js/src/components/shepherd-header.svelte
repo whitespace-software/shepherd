c0a810340b216e9f4a89557359d337f22e933835
Subhan-Nasir <i40210@verisk.com>
2025-07-29 17:19:50 +0100

    Header styling

--- shepherd.js/src/components/shepherd-header.svelte
+++ shepherd.js/src/components/shepherd-header.svelte
@@ -1,10 +1,15 @@
 <script lang="typescript">
-  import { Step, type StepOptionsCancelIcon, type StringOrStringFunction } from 'src/step';
-  import ShepherdCancelIcon from './shepherd-cancel-icon.svelte';
-  import ShepherdTitle from './shepherd-title.svelte';
+  import {
+    Step,
+    type StepOptionsCancelIcon,
+    type StringOrStringFunction
+  } from "src/step";
+  import ShepherdCancelIcon from "./shepherd-cancel-icon.svelte";
+  import ShepherdTitle from "./shepherd-title.svelte";
 
   export let labelId: string, step: Step;
-  let title: StringOrStringFunction | undefined, cancelIcon: StepOptionsCancelIcon | undefined;
+  let title: StringOrStringFunction | undefined,
+    cancelIcon: StepOptionsCancelIcon | undefined;
 
   $: {
     title = step.options.title;
@@ -24,17 +29,21 @@
 
 <style global>
   .shepherd-header {
-    align-items: center;
     border-top-left-radius: 5px;
     border-top-right-radius: 5px;
+
     display: flex;
     justify-content: flex-end;
+    align-items: center;
+
     line-height: 2em;
+
     padding: 0.75rem 0.75rem 0;
+
   }
 
   .shepherd-has-title .shepherd-content .shepherd-header {
-    background: #e6e6e6;
-    padding: 1em;
+    background: var(--tour-header-bg);
+    padding: 0.75rem 1.5rem;
   }
 </style>
